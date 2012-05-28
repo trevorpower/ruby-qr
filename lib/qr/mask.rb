70f2c2bf51ca12aa344614e4b9cefdd6b6ec594c
Trevor Power <trevor_power@yahoo.com>
2012-05-28 15:44:35 -0700

    add format code to mask implementaion

--- lib/qr/mask.rb
+++ lib/qr/mask.rb
@@ -1,8 +1,16 @@
 module QR
   class Mask < Layer
+    def initialize n, o
+      super n, o
+      @code = 0b001 
+    end
+    def format? i
+      return super i if i < 12
+      @code[i - 12]
+    end
     def module?(x, y)
-      return !@next.module?(x, y) if y % 2 == 0
-      super x, y
+      return super(x, y) unless y % 2 == 0
+      !super x, y
     end
   end
 end
