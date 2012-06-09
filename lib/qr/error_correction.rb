1de830c5accc4c11f2dfee6872508f7f7b1dfe5b
Trevor Power <trevor_power@yahoo.com>
2012-06-09 15:50:13 -0700

    fixed issue with nil values during poly division

--- lib/qr/error_correction.rb
+++ lib/qr/error_correction.rb
@@ -31,22 +31,31 @@ module QR
       super + ErrorCorrection.error_code(super)
     end
 
-    def self.error_code data
+    def self.galois_field size, prime_modulus
       gf = [1]
-
-      (1..255).map do |i|
+      (1..size - 1).map do |i|
         v = gf[i - 1] * 2
-        v = v ^ 285 if v > 255
+        v = v ^ prime_modulus if v > size - 1
         gf[i] = v
       end
+      gf
+    end
+
+    def self.generator_polynomial gf, size
+      #[0, 251, 67, 46, 61, 118, 70, 64, 94, 32, 45]
+      [0, 87, 229, 146, 149, 238, 102, 21]
+    end
+
+    def self.error_code data
+      gf = galois_field 2**8, 0b100011101
+
+      gen = generator_polynomial gf, 10
 
       poly = data
           .scan(%r/......../)
           .map{|i| i.to_i(2)}
           .map{|i| gf.index i}
-      (0..9).each{ poly << nil }
-
-      gen = [0, 251, 67, 46, 61, 118, 70, 64, 94, 32, 45]
+          .concat(Array.new(7, nil))
 
       while poly.size >= gen.size
         diff = poly.first - gen.first
@@ -54,7 +63,7 @@ module QR
           .zip(gen.map{|i| (i + diff) % 255})
           .map{|a,b| (a.nil? ? 0 : gf[a]) ^ (b.nil? ? 0 : gf[b])}
           .map{|i| gf.index i}
-          .drop(1)
+          .drop_while{|i| i.nil?}
       end
 
       poly
