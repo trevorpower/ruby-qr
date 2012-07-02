6eadc39daf4e25e5dc1176c171148437309c1633
Trevor Power <trevor_power@yahoo.com>
2012-07-02 15:45:34 +0100

    tidy mode class definition file

--- lib/qr/mode.rb
+++ lib/qr/mode.rb
@@ -1,19 +1,17 @@
-module QR
-  class Mode < Layer
-    
-    def initialize(lower_level, config, mode)
-      config[:data] = mode_bits(mode) + config[:data] 
-      super lower_level, config
-    end
-
-    def mode_bits mode
-      case mode
-        when :numeric then '0001'
-        when :alphanumeric then '0010'
-        when :byte then '0100'
-        when :eom then '0000'
-      end
-    end
-
+class QR::Mode < QR::Layer
+  
+  def initialize(lower_level, config, mode)
+    config[:data] = mode_bits(mode) + config[:data] 
+    super lower_level, config
   end
+
+  def mode_bits mode
+    case mode
+      when :numeric then '0001'
+      when :alphanumeric then '0010'
+      when :byte then '0100'
+      when :eom then '0000'
+    end
+  end
+
 end
