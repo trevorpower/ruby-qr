f8a9ba29149ac2b35bac146b163d20dacedd2243
Trevor Power <trevor_power@yahoo.com>
2012-07-02 13:28:02 +0100

    tidy up format layer

--- lib/qr/vertical_format.rb
+++ lib/qr/vertical_format.rb
@@ -1,19 +1,25 @@
-module QR
-  class VerticalFormat < Layer
-    def initialize(n, o)
-      super n, o
-    end
-    
-    def module?(x, y, config)
-      return super(x, y, config) if x != 8
-      return true if max - y == 7
-      if y < 8
-        config[:format][14 - y] == '1'
-      elsif max - y < 7
-        config[:format][max - y] == '1'
-      else
-        super x, y, config 
-      end
+class QR::VerticalFormat < QR::Layer
+
+  def initialize(n, o)
+    super n, o
+  end
+  
+  def module?(x, y, config)
+    format_module?(x, y, config[:format]) do
+      super x, y, config
     end
   end
+
+  def format_module? x, y, format
+    return yield if x != 8
+    return true if max - y == 7
+    if y < 8
+      format[14 - y] == '1'
+    elsif max - y < 7
+      format[max - y] == '1'
+    else
+      yield
+    end
+  end
+
 end
