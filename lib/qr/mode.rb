104578024b26463b6a5d8a7012bd5452414c578d
Trevor Power <trevor_power@yahoo.com>
2012-07-02 15:41:18 +0100

    move modes into mode layer

--- lib/qr/mode.rb
+++ lib/qr/mode.rb
@@ -1,22 +1,22 @@
 module QR
-  class Modes
-    def self.numeric
-      '0001'
-    end
-    def self.alphanumeric
-      '0010'
-    end
-    def self.byte
-      '0100'
-    end
-    def self.eom
-      '0000'
-    end
-  end
   class Mode < Layer
     def initialize(lower_level, config, mode)
-      config[:data] = QR::Modes.send(mode) + config[:data] 
+      config[:data] = QR::Mode::Modes.send(mode) + config[:data] 
       super lower_level, config
+    end
+    class Modes
+      def self.numeric
+        '0001'
+      end
+      def self.alphanumeric
+        '0010'
+      end
+      def self.byte
+        '0100'
+      end
+      def self.eom
+        '0000'
+      end
     end
   end
 end
