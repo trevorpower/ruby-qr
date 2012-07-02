9f5e3b98f8f1624314e4afef8f439c43b2a0a651
Trevor Power <trevor_power@yahoo.com>
2012-07-02 15:47:21 +0100

    moved end layer config into initialize

--- lib/qr/padding.rb
+++ lib/qr/padding.rb
@@ -2,8 +2,9 @@ require 'qr/layer'
 
 module QR
   class End < Layer
-    def configure config
+    def initialize lower_level, config
       config[:data] = config[:data] + "0000"
+      super lower_level, config
     end
   end
 end
