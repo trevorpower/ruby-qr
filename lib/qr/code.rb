bce757613a30a614c4a51af4e0fb08af1f2cf7ec
Trevor Power <trevor_power@yahoo.com>
2012-07-09 23:05:50 +0100

    moved position squares module to own file

--- lib/qr/code.rb
+++ lib/qr/code.rb
@@ -5,24 +5,6 @@ require 'math/bch'
 
 module QR
 
-  module PositionSquares
-    def module? x, y, max
-      transforms = [[x, y], [max - x, y], [x, max - y]]
-      transforms.select! do |x, y|
-        x < 8 && y < 8
-      end
-      if transforms.empty?
-        super
-      else
-        x, y = transforms.first 
-        return false if x == 7 || y == 7
-        return true if x == 0 || x == 6
-        return true if y == 0 || y == 6
-        x != 1 && y != 1 && x != 5 && y != 5
-      end
-    end
-  end
-
   module QuietZone
     def max
       super + quiet_zone_gap * 2
---
+++ lib/qr/position_squares.rb
@@ -0,0 +1,20 @@
+module QR::PositionSquares
+  def module? x, y, max
+    transforms = [[x, y], [max - x, y], [x, max - y]]
+
+    transforms.select! do |x, y|
+      x < 8 && y < 8
+    end
+
+    if transforms.empty?
+      super
+    else
+      x, y = transforms.first 
+      return false if x == 7 || y == 7
+      return true if x == 0 || x == 6
+      return true if y == 0 || y == 6
+      x != 1 && y != 1 && x != 5 && y != 5
+    end
+  end
+end
+
