56db27708ff1737c1242ad2207dad53025519384
Trevor Power <trevor_power@yahoo.com>
2012-07-13 20:59:06 +0100

    fixed some test code and added rake file

--- lib/math/bch.rb
+++ lib/math/bch.rb
@@ -1,12 +1,15 @@
 require 'math/polynomial'
 
-module QR::Math
-  def self.bch(bits)
-    bits = bits.ljust 5, "0"
-    poly = bits.chars.map{|c| c.to_i}
-    poly.fill 0, 5, 10
-    generator = [1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1]
-    q, r = Math::Polynomial.div(poly, generator)
-    r.map{|i| i*i}.map{|i| i % 2}.join.rjust 10, "0"
+module QR
+  module Math
+    def self.bch(bits)
+      bits = bits.ljust 5, "0"
+      poly = bits.chars.map{|c| c.to_i}
+      poly.fill 0, 5, 10
+      generator = [1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1]
+      q, r = Math::Polynomial.div(poly, generator)
+      r.map{|i| i*i}.map{|i| i % 2}.join.rjust 10, "0"
+    end
   end
 end
+  
