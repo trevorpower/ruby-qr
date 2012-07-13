e1d4d2320b7a3e6218e1969d6a7ea87c50abea97
Trevor Power <trevor_power@yahoo.com>
2012-07-13 21:19:52 +0100

    fixed format mask test

--- test/test_format_mask.rb
+++ test/test_format_mask.rb
@@ -2,13 +2,18 @@ require 'test_helper'
 require 'qr/format_mask'
 
 class TestFormatMask < MiniTest::Unit::TestCase
+
   def setup
-    @default = DefaultLayer.new
-    @layer = QR::FormatMask.new @default, nil
+    extend QR::FormatMask
+  end
+
+  def format
+    @default
   end
 
   def test_that_format_is_masked
-    @default.format = '001010011011100'
-    assert_equal '100000011001110', @layer.format
+    @default = '001010011011100'
+    assert_equal '100000011001110', format
   end
+
 end
