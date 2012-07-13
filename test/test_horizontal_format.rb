c965d65b8a2f95af3f5233a7d3f528271cccba04
Trevor Power <trevor_power@yahoo.com>
2012-07-13 21:27:18 +0100

    fixed some errors wiht test horizontal status

--- test/test_horizontal_format.rb
+++ test/test_horizontal_format.rb
@@ -3,40 +3,49 @@ require 'qr/horizontal_format'
 
 class TestHorizontalFormat < MiniTest::Unit::TestCase
 
+  attr_accessor :max
+
   def setup
-    @default = DefaultLayer.new
-    @layer = QR::HorizontalFormat.new(@default, nil)
+    extend QR::HorizontalFormat
+  end
+
+  def format
+    @default
+  end
+
+  def module? x, y, max
+    false
   end
 
   def test_that_first_five_bits_on_row_eight_correspond_to_first_five_bits_of_format
-    @default.format = "010010"
-    assert_equal false, @layer.module?(0, 8)
-    assert_equal true, @layer.module?(1, 8)
-    assert_equal false, @layer.module?(2, 8)
-    assert_equal false, @layer.module?(3, 8)
-    assert_equal true, @layer.module?(4, 8)
-    assert_equal false, @layer.module?(5, 8)
+    @default = "010010"
+    assert_equal false, module?(0, 8, 21)
+    assert_equal true, module?(1, 8, 21)
+    assert_equal false, module?(2, 8, 21)
+    assert_equal false, module?(3, 8, 21)
+    assert_equal true, module?(4, 8, 21)
+    assert_equal false, module?(5, 8, 21)
   end
 
   def test_that_module_seven_on_row_eight_is_true_when_bit_six_is_one
-    @default.format = "0000001"
-    assert_equal true, @layer.module?(7, 8)
+    @default = "0000001"
+    assert_equal true, module?(7, 8, 21)
   end
 
   def test_that_module_seven_on_row_eight_is_false_when_bit_six_is_one
-    @default.format = "0000000"
-    assert_equal false, @layer.module?(7, 8)
+    @default = "0000000"
+    assert_equal false, module?(7, 8, 21)
   end
 
   def test_that_last_six_bits_on_row_eight_correspond_to_first_six_bits_of_format
-                      #012345678901234
-    @default.format = "000000000001001"
-    @default.max = 39
-    assert_equal false, @layer.module?(34, 8)
-    assert_equal false, @layer.module?(35, 8)
-    assert_equal true, @layer.module?(36, 8)
-    assert_equal false, @layer.module?(37, 8)
-    assert_equal false, @layer.module?(38, 8)
-    assert_equal true, @layer.module?(39, 8)
+               #012345678901234
+    @default = "000000000001001"
+    max = 39
+    assert_equal false, module?(34, 8, 21)
+    assert_equal false, module?(35, 8, 21)
+    assert_equal true, module?(36, 8, 21)
+    assert_equal false, module?(37, 8, 21)
+    assert_equal false, module?(38, 8, 21)
+    assert_equal true, module?(39, 8, 21)
   end
 end
