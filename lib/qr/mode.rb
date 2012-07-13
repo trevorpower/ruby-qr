d11a166f7976533e906ca0774a7c1f094a5cece0
Trevor Power <trevor_power@yahoo.com>
2012-07-13 14:55:36 +0100

    added mac_words to version modules

--- lib/qr/mode.rb
+++ lib/qr/mode.rb
@@ -7,10 +7,6 @@ module QR
         '0100' + super
       end
 
-      def word_size
-        8
-      end
-
     end
 
   end
--- lib/qr/versions.rb
+++ lib/qr/versions.rb
@@ -1,17 +1,28 @@
 module QR
 
-  Versions = (1..40).map do |version|
+  Versions = []
 
-    Module.new do
+  def self.version version, words
+
+    Versions << Module.new do
       module_eval %Q"
 
         def max
           #{version * 4 + 16}
         end
 
+        def max_words
+          #{words}
+        end
       "
     end
 
   end
+
+  version 1, 24
+  version 2, 44
+  version 3, 70
+  version 4, 100
+  version 5, 134
 
 end
